1e158c5c512587361c41ef26c07049c807abe8f2
dove <3364975512@qq.com>
2024-11-28 22:26:31 +0800

    version2
    70%

---
+++ src/main/java/Sokoban/Model/Board.java
@@ -0,0 +1,22 @@
+package Sokoban.Model;
+
+public class Board {
+    private int currentRow;
+    private int currentCol;
+
+    public int getCurrentRow() {
+        return currentRow;
+    }
+
+    public void setCurrentRow(int currentRow) {
+        this.currentRow = currentRow;
+    }
+
+    public int getCurrentCol() {
+        return currentCol;
+    }
+
+    public void setCurrentCol(int currentCol) {
+        this.currentCol = currentCol;
+    }
+}
