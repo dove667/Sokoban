0bf49674aca2a10c31ab3ed07b49991f1310f024
dove <3364975512@qq.com>
2024-11-28 13:35:16 +0800

    初始化

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module Sodoban {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
+    opens org.example.demo1 to javafx.fxml;
+    opens Sokoban to javafx.fxml, javafx.graphics, javafx.controls,java.base;
+    exports org.example.demo1;
+    exports Sokoban;
+    exports Sokoban.Controller;
+    opens Sokoban.Controller to java.base, javafx.controls, javafx.fxml, javafx.graphics;
+    exports Sokoban.Model;
+    opens Sokoban.Model to java.base, javafx.controls, javafx.fxml, javafx.graphics;
+}
