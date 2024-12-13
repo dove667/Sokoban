6048215072220bd79f0185a576c39a93131e5db3
Aperitas <dbowen2024@163.com>
2024-12-13 23:51:21 +0800

    ？？不知道哪来的修改

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,7 @@ module Sodoban {
     requires javafx.base;
     requires java.desktop;
     requires annotations;
-    opens org.example.demo1 to javafx.fxml;
     opens Sokoban to javafx.fxml, javafx.graphics, javafx.controls,java.base;
-    exports org.example.demo1;
     exports Sokoban;
     exports Sokoban.Controller;
     opens Sokoban.Controller to java.base, javafx.controls, javafx.fxml, javafx.graphics;
