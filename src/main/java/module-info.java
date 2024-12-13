dfb34f7b4e0bb714c0e938f461ff52859975299e
dove <3364975512@qq.com>
2024-12-13 18:53:52 +0800

    保存一下

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,9 @@ module Sodoban {
     requires javafx.base;
     requires java.desktop;
     requires annotations;
-    opens org.example.demo1 to javafx.fxml;
+
     opens Sokoban to javafx.fxml, javafx.graphics, javafx.controls,java.base;
-    exports org.example.demo1;
+
     exports Sokoban;
     exports Sokoban.Controller;
     opens Sokoban.Controller to java.base, javafx.controls, javafx.fxml, javafx.graphics;
